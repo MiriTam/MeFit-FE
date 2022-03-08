0870f7ae0fbf464e7023280b429b26547d126ba7
AmmarAhmed007 <ammar.ahmed@no.experis.com>
2022-03-08 14:40:39 +0100

    FEAT: Implemented RegisterForm UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import Login from './pages/Login';
+import RegisterForm from './components/RegisterForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
 
 function App() {
 	return (
-		<>
-			<h2>Sign in Page</h2>
-			<Login />
-		</>
+		<BrowserRouter>
+			<div className='App'>
+				<Routes>
+					<Route path='/' element={<LoginForm />} />
+          <Route path='/register' element={<RegisterForm />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
