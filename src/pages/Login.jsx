9397039e283f3089bf677f9153a010f86d96b0c0
AmmarAhmed007 <ammar.ahmed@no.experis.com>
2022-03-08 13:52:18 +0100

    FEAT: Implemented login UI with material ui

---
+++ src/pages/Login.jsx
@@ -0,0 +1,80 @@
+import {
+	Avatar,
+	Button,
+	Checkbox,
+	Container,
+	createTheme,
+	CssBaseline,
+	FormControlLabel,
+	Grid,
+	Link,
+	TextField,
+	ThemeProvider,
+	Typography
+} from '@mui/material';
+import { Box } from '@mui/system';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+
+const Login = () => {
+	const theme = createTheme();
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component='main' maxWidth='xs'>
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center'
+					}}>
+					<Avatar sx={{ m: 1, bgcolor: 'secondary-main' }}>
+						<FitnessCenterIcon />
+					</Avatar>
+					<Typography component='h1' variant='h5'>
+						Sign in
+					</Typography>
+					<Box component='form' noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							id='email'
+							label='Email Address'
+							name='email'
+							autoComplete='email'
+							autoFocus
+						/>
+						<TextField
+							margin='normal'
+							required
+							fullWidth
+							name='password'
+							label='Password'
+							type='password'
+							id='password'
+							autoComplete='current-password'
+						/>
+						<FormControlLabel
+							control={<Checkbox value='remember' color='primary' />}
+							label='Remember me'
+						/>
+					</Box>
+					<Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+						Sign in
+					</Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                {"Need an account? Click here."}
+                                </Link>
+                        </Grid>
+                    </Grid>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
+};
+
+export default Login;
