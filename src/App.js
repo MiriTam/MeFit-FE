45fd02590b130aa2e74489c6074c8bcffab551f1
konstapascal <pascalkonstantinos@gmail.com>
2022-03-08 12:52:23 +0100

    CHORE: Cleaned up repo, installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import AddBox from '@mui/icons-material/AddBox';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<p className=''>This is a paragraph.</p>
+			<Button size='large' disabled={false} variant='contained' color='primary'>
+				Primary Button
+			</Button>
+			<AddBox color='primary' />
+		</div>
+	);
 }
 
 export default App;
