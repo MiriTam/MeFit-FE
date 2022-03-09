c03cac0b2141f0997e62907906670722d4d939cd
konstapascal <pascalkonstantinos@gmail.com>
2022-03-09 21:28:22 +0100

    chore: changes

--- src/api/programs.js
+++ src/api/programs.js
@@ -3,12 +3,32 @@ const programs = [
 		id: 1,
 		name: 'Excercise 1',
 		description: 'Excercise 1 description',
-		excercises: ['Pull up', 'Bench press']
+		excercises: ['Excercise 1', 'Excercise 2']
 	},
-	{ id: 1, name: 'Excercise 2', description: 'Excercise 2 description' },
-	{ id: 1, name: 'Excercise 3', description: 'Excercise 3 description' },
-	{ id: 1, name: 'Excercise 4', description: 'Excercise 4 description' },
-	{ id: 1, name: 'Excercise 5', description: 'Excercise 5 description' }
+	{
+		id: 2,
+		name: 'Excercise 2',
+		description: 'Excercise 2 description',
+		excercises: ['Excercise 1', 'Excercise 2']
+	},
+	{
+		id: 3,
+		name: 'Excercise 3',
+		description: 'Excercise 3 description',
+		excercises: ['Excercise 1', 'Excercise 2']
+	},
+	{
+		id: 4,
+		name: 'Excercise 4',
+		description: 'Excercise 4 description',
+		excercises: ['Excercise 1', 'Excercise 2']
+	},
+	{
+		id: 5,
+		name: 'Excercise 5',
+		description: 'Excercise 5 description',
+		excercises: ['Excercise 1', 'Excercise 2']
+	}
 ];
 
 export default function getPrograms() {
