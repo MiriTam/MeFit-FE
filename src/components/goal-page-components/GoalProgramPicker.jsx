eb825c36a09560c5d0c3945952f732ba1151337a
konstapascal <pascalkonstantinos@gmail.com>
2022-03-29 00:01:30 +0200

    leftover file from prev commit

--- src/components/goal-page-components/GoalProgramPicker.jsx
+++ src/components/goal-page-components/GoalProgramPicker.jsx
@@ -1,8 +1,9 @@
-import { usePrograms } from '../../context/ProgramsContext';
-import ProgramCard from '../programs-page-components/ProgramCard';
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
+
+import { usePrograms } from '../../context/ProgramsContext';
+import ProgramCard from '../programs-page-components/ProgramCard';
 
 const GoalProgramPicker = () => {
 	const { programs } = usePrograms();
