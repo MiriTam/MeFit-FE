d3bc4761c002437e69ac28e347debf854d810f4f
Miriam T.G. Aarag <miriam.aarag@no.experis.com>
2022-03-30 16:39:47 +0200

    Added animation to authentication page.

--- src/components/authentication-page-components/AuthenticationForm.jsx
+++ src/components/authentication-page-components/AuthenticationForm.jsx
@@ -2,6 +2,8 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Button, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { ClipLoader } from 'react-spinners';
+import Lottie from 'lottie-react';
+import animation from './../../animations/start-anim';
 
 const AuthenticationForm = () => {
 	const { loginWithRedirect, isLoading } = useAuth0();
@@ -10,21 +12,30 @@ const AuthenticationForm = () => {
 		<Box
 			className='text-center'
 			sx={{
-				marginTop: 14
+				marginTop: 3
 			}}>
 			<Typography component='h1' variant='h4'>
-				MeFit FE
+				MeFit
 			</Typography>
+		    <Lottie
+			    animationData={animation}
+				loop={true}
+			    height={300}
+				width={300}
+			/>
 			<Box>
 				<Typography color='text.secondary' sx={{ mt: 1, fontSize: 20 }}>
-					Press the Authenticate button to start Auth0 authentication to MeFit.
+					Let's get fit together!
+				</Typography>
+				<Typography color='text.secondary' sx={{ mt: 1, fontSize: 20 }}>
+					Press the button below to be re-directed to login.
 				</Typography>
 			</Box>
 			<Button onClick={() => loginWithRedirect()} fullWidth variant='contained' sx={{ mt: 4 }}>
-				Authenticate
+				Get started
 			</Button>
 			<Box className='mt-10'>{isLoading && <ClipLoader color='#2374CE' size={30} />}</Box>
-		</Box>
+			</Box>
 	);
 };
 
