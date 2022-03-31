8a48c09ec92b2f94d564ad2cc1b92df094fbac6b
Miriam T.G. Aarag <miriam.aarag@no.experis.com>
2022-03-31 16:18:43 +0200

    Added inspirational animation.

--- src/components/dashboard-page-components/DashboardSetGoal.jsx
+++ src/components/dashboard-page-components/DashboardSetGoal.jsx
@@ -1,18 +1,23 @@
 import { Box, CardContent, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import chillingAnimation from './../../animations/chilling';
 
 export default function DashboardSetGoal() {
 	return (
 		<Box className='shadow-md  rounded-md p-6 '>
 			<CardContent>
-				<Typography sx={{ mb: 2, fontSize: 30 }} variant='h4'>
-					You have no goal set
-				</Typography>
 				<Link to={'/goals'}>
 					<Typography variant='h5' className='text-blue-600 underline'>
 						Make goal
-					</Typography>
+						</Typography>
 				</Link>
+				<Box sx={{width: '500px', margin: 'auto'}}>
+				    <Lottie
+					    animationData={chillingAnimation}
+					    loop={true}
+					/>
+				</Box>
 			</CardContent>
 		</Box>
 	);
